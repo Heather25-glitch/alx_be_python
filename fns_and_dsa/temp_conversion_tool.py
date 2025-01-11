683cc301e387e82a29d25b66a444a19ff29f6f95
Heather Selim <heatherselim25@gmail.com>
2025-01-11 18:59:33 +0200

    last

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,18 +1,18 @@
 # Define Global Conversion Factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
-FREEZING_POINT_FAHRENHEIT = 32
+
 
 # Function to convert Fahrenheit to Celsius
 def convert_to_celsius(fahrenheit):
     # Use the global factor for conversion
-    celsius = (fahrenheit - FREEZING_POINT_FAHRENHEIT) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     return celsius
 
 # Function to convert Celsius to Fahrenheit
 def convert_to_fahrenheit(celsius):
     # Use the global factor for conversion
-    fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + FREEZING_POINT_FAHRENHEIT
+    fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
     return fahrenheit
 
 # Main function to handle user input and call the appropriate conversion function
